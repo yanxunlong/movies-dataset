9288d8a52c262fd7f4dfcbe05dd0c5d01500668a
yanxunlong <167728341+yanxunlong@users.noreply.github.com>
2025-01-23 13:41:16 +0000

    new changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,47 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import requests
+import os
+
+def download_model(url, output_path):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  # Raise an error for bad HTTP responses
+        with open(output_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+        st.write("Model downloaded successfully.")
+    except Exception as e:
+        st.error(f"Error downloading model: {e}")
+
+model_url = "https://drive.google.com/uc?export=download&id=1WxTfnHN_WjMiNEVE0tQIHwyWcy1qLxen"
+feature_url = "https://drive.google.com/uc?export=download&id=1m_7xJOl0I3Oyweu1k65sJG122w1cH-qr"
+model_path = "data/final_rf_model.pkl"
+feature_path = "data/final_rf_model_features.pkl"
+
+if not os.path.exists(model_path):
+    st.write("Downloading model...")
+    download_model(model_url, model_path)
+
+if not os.path.exists(feature_path):
+    st.write("Downloading feature names...")
+    download_model(feature_url, feature_path)
 
 try:
-    model = joblib.load("data/final_rf_model.pkl")
-    feature_names = joblib.load("data/final_rf_model_features.pkl") 
-    st.write("Model and feature names loaded successfully1111!")
+    model = joblib.load(model_path)
+    feature_names = joblib.load(feature_path)
+    st.write("Model and feature names loaded successfully!")
 except Exception as e:
     st.error(f"Error loading model or feature names: {e}")
 
 st.title("Car Price Prediction App")
 st.write("Enter the car details below to predict the price in Euros.")
 
-power_kw = st.number_input("Power (kW):", min_value=0.0, step=10)
-power_ps = st.number_input("Power (PS):", min_value=0.0, step=10)
-fuel_consumption = st.number_input("Fuel Consumption (g/km):", min_value=0.0, step=10)
+power_kw = st.number_input("Power (kW):", min_value=0.0, step=10.0)
+power_ps = st.number_input("Power (PS):", min_value=0.0, step=10.0)
+fuel_consumption = st.number_input("Fuel Consumption (g/km):", min_value=0.0, step=10.0)
 mileage_in_km = st.number_input("Mileage (in km):", min_value=0.0, step=100.0)
 car_age = st.number_input("Car Age (Years):", min_value=0, step=1)
 
@@ -68,9 +95,9 @@ if st.button("Predict"):
 
         for col in feature_names:
             if col not in input_data.columns:
-                input_data[col] = 0  
+                input_data[col] = 0
 
-        input_data = input_data[feature_names] 
+        input_data = input_data[feature_names]
 
         prediction = model.predict(input_data)[0]
 
