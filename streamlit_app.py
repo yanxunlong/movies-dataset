a0aee32151a579c8a6d49d0a5ed2bf996c2c2155
yanxunlong <167728341+yanxunlong@users.noreply.github.com>
2025-01-23 21:09:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ except Exception as e:
 st.title("Car Price Prediction App")
 st.write("Enter the car details below to predict the price in Euros.")
 
-power_kw = st.number_input("Power (kW):", min_value=0.0, step=0.1)
-power_ps = st.number_input("Power (PS):", min_value=0.0, step=0.1)
-fuel_consumption = st.number_input("Fuel Consumption (g/km):", min_value=0.0, step=0.1)
+power_kw = st.number_input("Power (kW):", min_value=0.0, step=20)
+power_ps = st.number_input("Power (PS):", min_value=0.0, step=20)
+fuel_consumption = st.number_input("Fuel Consumption (g/km):", min_value=0.0, step=10)
 mileage_in_km = st.number_input("Mileage (in km):", min_value=0.0, step=100.0)
 car_age = st.number_input("Car Age (Years):", min_value=0, step=1)
 
